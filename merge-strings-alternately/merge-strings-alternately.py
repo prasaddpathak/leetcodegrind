0d8e997e4e1f3f2cbfe3d1c126f336217720231c
Prasad Pathak <prasaddpathak@gmail.com>
2023-06-11 20:32:56 -0700

    Time: 46 ms (27.86%) | Memory: 16.4 MB (13.19%) - LeetSync

---
+++ merge-strings-alternately/merge-strings-alternately.py
@@ -0,0 +1,5 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        min_len = min(len(word1), len(word2))
+        return "".join([word1[i] + word2[i] for i in range(min_len)]) \
+            + word1[min_len:] + word2[min_len:]
